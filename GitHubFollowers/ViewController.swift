807a75384c3ee32a2f1c8edfd13680858d03d6f3
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-06 06:33:04 -0500

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Chad Rutherford on 2/6/20.
+//  Copyright © 2020 chadarutherford.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
