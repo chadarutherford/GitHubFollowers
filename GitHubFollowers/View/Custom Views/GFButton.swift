600d7e7944713fe48b598792c6d580c0b5482c06
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-06 08:33:39 -0500

    Created Custom Views

---
+++ GitHubFollowers/View/Custom Views/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Chad Rutherford on 2/6/20.
+//  Copyright © 2020 chadarutherford.com. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GitHubFollowers/View/Custom Views/GFTextField.swift
@@ -0,0 +1,53 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Chad Rutherford on 2/6/20.
+//  Copyright © 2020 chadarutherford.com. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a Username"
+    }
+}
+
+struct GFTextFieldPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+    }
+    
+    struct ContainerView: UIViewRepresentable {
+        func updateUIView(_ uiView: GFTextFieldPreview.ContainerView.UIViewType, context: UIViewRepresentableContext<GFTextFieldPreview.ContainerView>) {
+            
+        }
+        
+        func makeUIView(context: UIViewRepresentableContext<GFTextFieldPreview.ContainerView>) -> GFTextField {
+            GFTextField()
+        }
+    }
+}
